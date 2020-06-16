c2ea31c0c6129229eec87e58982a86dfc5c28a71
Isale <isale.bryan@gmail.com>
2020-06-16 11:53:09 +0300

    Implement the Serialization of a Passport User

--- server.js
+++ server.js
@@ -4,12 +4,23 @@ const express = require("express");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const path = require('path');
 const passport = require('passport');
+const mongo = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 const session = require('express-session');
 require('dotenv').config()
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set method to assign 'pug' as the 'view-engine'.
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views/pug'))// sets the view directory
+
 
 // Create a session middleware with the given options
 app.use(session({
@@ -24,28 +35,31 @@ app.use(passport.initialize());
 // enable session support
 app.use(passport.session());
 
-const ObjectID = require('mongodb').ObjectID;
+mongo.connect(process.env.DATABASE,{ useUnifiedTopology: true }, (err, db) => {
+  if (err) {
+    console.log('Database error: ' + err);
+    
+  } else {
+    console.log('Successful database connection');
 
-passport.serializeUser((user, done)=> {
-  done(null, user._id);
-})
+    passport.serializeUser((user, done)=> {
+      done(null, user._id);
+    });
+    
+  }
+});
+
+
 passport.deserializeUser((id, done)=> {
-  // db.collection('users').findOne({
-  //   _id: new ObjectID(id)
-  // },
-  // (err, doc) => {
-  //   done(null, doc);
-  // })
-  done(null, null)
+  db.collection('users').findOne({
+    _id: new ObjectID(id)
+  },
+  (err, doc) => {
+    done(null, doc);
+  })
+  
 })
 
-app.use("/public", express.static(process.cwd() + "/public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// set method to assign 'pug' as the 'view-engine'.
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views/pug'))// sets the view directory
 
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
