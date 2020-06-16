25a28a4e5dda7c962ec3348d674d62311551c189
Isale <isale.bryan@gmail.com>
2020-06-16 11:08:36 +0300

    set up pug

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const path = require('path');
 
 const app = express();
 
@@ -10,11 +11,16 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// set method to assign 'pug' as the 'view-engine'.
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views/pug'))// sets the view directory
+
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
+  res.render('index'); // render view file on this endpoint
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
