dcb8c76edd0a4fe73aa7e841fae6df20cbd1c748
Isale <isale.bryan@gmail.com>
2020-06-17 16:27:40 +0300

    Logging a User Out

--- server.js
+++ server.js
@@ -85,16 +85,43 @@ mongo.connect(process.env.DATABASE,{ useUnifiedTopology: true }, (err, client) =
     ));
     
     
-  }
-});
-
 
 
 
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
-  res.render('index', {title: 'Hello', message: 'Please login', showLogin: true}); // render view file on this endpoint
+  res.render('index', {
+    title: 'Hello',
+    message: 'Please login',
+    showLogin: true,
+  showRegistration: true
+}); // render view file on this endpoint
 });
+
+// registering a new user
+app.route('/register')
+.post((req, res, next)=>{
+  db.collection('users').findOne({ username: req.body.username },function (err, user) {
+    if (err) {
+      next(err);
+    } else if (user) {
+      res.redirect('/');
+    } else {
+      db.collection('users').insertOne({
+        username: req.body.username,
+        password: req.body.password
+      },
+      (err, doc) => {
+        if (err) {
+          res.redirect('/');
+        } else {
+          next(null, user);
+        }
+      })
+    }
+  })
+})
+
 
 app
 .route('/login')
@@ -108,6 +135,18 @@ app
   res.render(process.cwd() + '/views/pug/profile',{ username: req.user.username })
 })
 
+app.route('/logout')
+.get((req, res) => {
+  req.logout();
+  res.redirect('/')
+});
+// handling missing pages (404)
+app.use((req, res, next)=>{
+  res.status(404)
+  .type('text')
+  .send('Not found')
+})
+
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next()
@@ -116,6 +155,17 @@ function ensureAuthenticated(req, res, next) {
   }
 
 }
+passport.authenticate('local', {failureRedirect: '/'}), 
+(req, res, next) => {
+  res.redirect('/profile')
+}
+
+    
+  }
+});
+
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
